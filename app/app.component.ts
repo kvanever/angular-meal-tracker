ef215dca3f5402eb636a5266375933dfe336b347
Kevin VanEvery <kvanever@gmail.com>
2016-08-12 06:59:40 -0700

    add form to add meal

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from 'angular2/core';
 import { Meal } from './meal.model';
 import { MealListComponent } from './meal-list.component';
+import { AddNewMealComponent } from './add-new-meal.component';
 
 @Component({
   selector: 'my-app',
-  directives: [MealListComponent],
+  directives: [MealListComponent, AddNewMealComponent],
   template: `
     <meal-list [mealList]="meals"></meal-list>
+    <add-new-meal (onAddNewMeal)="createMeal($event)"></add-new-meal>
   `
 })
 export class AppComponent {
@@ -17,5 +19,11 @@ export class AppComponent {
       new Meal("Lunch", "Sandwich", 200, 1),
       new Meal("Dinner", "Pasta", 700, 2)
     ];
+  }
+  createMeal(name: string, description: string, calories: string): void {
+    var intCalories = parseInt(calories)
+    this.meals.push(
+      new Meal(name, description, intCalories, this.meals.length)
+    );
   }
 }
