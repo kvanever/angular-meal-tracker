ac52afeae67b8db9162c02e4cb5e900b59b4c9f0
Kevin VanEvery <kvanever@gmail.com>
2016-08-12 03:43:06 -0700

    add meal list to app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from 'angular2/core';
 import { Meal } from './meal.model';
+import { MealListComponent } from './meal-list.component';
 
 @Component({
   selector: 'my-app',
+  directives: [MealListComponent],
   template: `
-    <h1>Meal Tracker!</h1>
+    <meal-list></meal-list>
   `
 })
 
