a17baf9040715d94d62013f3479c981b910c7095
Kevin VanEvery <kvanever@gmail.com>
2016-08-12 04:52:55 -0700

    refactor again to workaround tr bug by removing meal component into meal list

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -1,33 +1,32 @@
 import { Component } from 'angular2/core';
 import { Meal } from './meal.model';
-import { MealComponent } from './meal.component';
 
 @Component({
   selector: 'meal-list',
   inputs: ['mealList'],
-  directives: [MealComponent],
   template: `
-    <div class="row">
-      <div class="col-md-1">
-        <h2>ID</h2>
-      </div>
-      <div class="col-md-3">
-        <h2>Name</h2>
-      </div>
-      <div class="col-md-4">
-        <h2>Description</h2>
-      </div>
-      <div class="col-md-2">
-        <h2>Calories</h2>
-      </div>
-      <div class="col-md-1">
-        <h2>Edit</h2>
-      </div>
-      <div class="col-md-1">
-        <h2>Delete</h2>
-      </div>
-    </div>
-    <meal-display *ngFor="#currentMeal of mealList" [meal]="currentMeal"></meal-display>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>ID#</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Calories</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr *ngFor="#meal of mealList">
+        <td>{{ meal.id }}</td>
+        <td>{{ meal.name }}</td>
+        <td>{{ meal.description }}</td>
+        <td>{{ meal.calories }}</td>
+        <td>Edit</td>
+        <td>Delete</td>
+      </tr>
+    </tbody>
+  </table>
   `
 })
 export class MealListComponent {
